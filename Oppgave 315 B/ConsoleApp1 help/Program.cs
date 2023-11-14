b48585438327b28c4e93c48b14a9b616664d4d88
martinsletsjoe <106916526+martinsletsjoe@users.noreply.github.com>
2023-11-14 10:40:48 +0100

    a

---
+++ Oppgave 315 B/ConsoleApp1 help/Program.cs
@@ -0,0 +1,14 @@
+﻿var range = 250;
+var counts = new int[range];
+string text = "something";
+
+while(!string.IsNullOrWhiteSpace(text))
+    for (int i = 0; i < text.Length; i++)
+    {
+        counts[text[i]]++;
+        if (counts[text[i]] > 0)
+        {
+            var character = (char)i;
+            Console.WriteLine($"{character} - {counts[i]}");
+        }
+    }
---
+++ Oppgave 315 B/Oppgave 315 B/Program.cs
@@ -0,0 +1,20 @@
+﻿var range = 250;
+var counts = new int[range];
+string text = "something";
+while (!string.IsNullOrWhiteSpace(text))
+{
+    text = Console.ReadLine().ToLower() ?? string.Empty;
+    for (int i = 0; i < text.Length; i++)
+        //foreach (var character in text ?? string.Empty)
+    {
+        counts[text[i]]++;
+        //counts[character]++;
+        //Console.WriteLine(counts[text[i]]);
+
+        if (counts[text[i]] > 0)
+        {
+            var character = text[i];
+            Console.WriteLine(character + " - " + counts[text[i]]);
+        }
+    }
+}asd
