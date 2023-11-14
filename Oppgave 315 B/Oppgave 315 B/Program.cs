6b9da68d8938e8bc2e85366a88c045f6edfeb199
martinsletsjoe <106916526+martinsletsjoe@users.noreply.github.com>
2023-11-14 10:48:30 +0100

    a

--- Oppgave 315 B/Oppgave 315 B/Program.cs
+++ Oppgave 315 B/Oppgave 315 B/Program.cs
@@ -3,7 +3,7 @@ var counts = new int[range];
 string text = "something";
 while (!string.IsNullOrWhiteSpace(text))
 {
-    text = Console.ReadLine().ToLower() ?? string.Empty;
+    text = Console.ReadLine().ToLower() ?? string.Empty;    
     for (int i = 0; i < text.Length; i++)
         //foreach (var character in text ?? string.Empty)
     {
@@ -17,4 +17,4 @@ while (!string.IsNullOrWhiteSpace(text))
             Console.WriteLine(character + " - " + counts[text[i]]);
         }
     }
-}asd
+}
