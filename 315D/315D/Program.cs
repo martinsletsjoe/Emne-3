b125494442751ec27359c64182dbd9e00af9290f
martinsletsjoe <106916526+martinsletsjoe@users.noreply.github.com>
2023-11-17 14:56:14 +0100

    fredag

---
+++ 315D/315D/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _315D
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv noe tekst");
+            var userAnswer = Console.ReadLine();
+            string[] listedWords = userAnswer.Split(' ');
+            var longestWord = "";
+
+            Console.WriteLine($"Det er {listedWords.Length} ord i teksten din");
+            foreach (var word in listedWords)
+            {
+
+                if (word.Length > longestWord.Length)
+                {
+                    longestWord = word;
+                }
+            }
+            Console.WriteLine($"{longestWord} er det lengste ordet i teksten");
+        }
+    }
+}
+//split('')
--- Oppgave 315 C/Oppgave 315 C/Program.cs
+++ Oppgave 315 C/Oppgave 315 C/Program.cs
@@ -1,57 +1,15 @@
-﻿using Console = System.Console;
-
-namespace Oppgave_315_C
+﻿namespace oppgave_315_C
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("PasswordGenerator");
-            Console.WriteLine("Options");
-            string[] passwordCriteria =
-                { "l = liten bokstav", "L = stor bokstav", "d = siffer", "s = spesialtegn (!\"#%¤&/(){}[]" };
-            foreach (var character in passwordCriteria)
+            Console.WriteLine("Skriv inn navnet ditt");
+            var inputName = Console.ReadLine();
+            for (int i = inputName.Length-1; i >= 0; i--)
             {
-                Console.WriteLine($"- {character}");
-            }
-
-            Console.WriteLine($"Eksempel: PasswordGenerator 14 lLssd");
-            Console.WriteLine("     betyr");
-            string[] explanation = { "1 liten bokstav", "1 stor bokstav", "2 spesialtegn", "2 sifre" };
-            foreach (var VARIABLE in explanation)
-            {
-                Console.WriteLine($"Min. {VARIABLE}");
-            }
-
-            Console.WriteLine($"Lengde på passordet skal være 14 tegn.");
-            Console.Write("Skriv inn ønsket lengde:");
-            string amountInput = Console.ReadLine();
-            foreach (var c in amountInput)
-            {
-                if (!char.IsDigit(c))
-                {
-                    Console.WriteLine("ugyldig, kun tall er gyldig");
-                }
-            }
-
-            Console.WriteLine("Hvilken egenskaper ønsker du at passordet skal ha?");
-            string typeInput = Console.ReadLine();
-            foreach (var c in typeInput)
-            {
-
-            }
-            {
-                
-            }
-            {
-                
+                Console.Write($"{inputName[i]}");
             }
         }
     }
 }
-//Console.WriteLine("Enter length of password");
-//string amountInput = Console.ReadLine();
-//int amount = int.Parse(amountInput);
-
-//Console.WriteLine("Enter password criteria");
-//string type = Console.ReadLine();
