d5a15b57c551cda1f2b8993a01e3313007d10063
martinsletsjoe <106916526+martinsletsjoe@users.noreply.github.com>
2023-11-27 15:31:38 +0100

    reeeee

--- oppgave 316A/oppgave 316A/Program.cs
+++ oppgave 316A/oppgave 316A/Program.cs
@@ -4,21 +4,60 @@
     {
         static void Main(string[] args)
         {
+            var words = GetWords();
+
+            GetWordEnding(words);
+
+        }
+
+        private static void GetWordEnding(string[] words)
+        {
+            Random random = new Random();
+            var randomWordIndex = random.Next(words.Length);
+            var word = words[randomWordIndex];
+
+            var randomWordEnd = new List<string>();
+            string c;
+            for (int i = word.Length - 3; i < word.Length; i++)
+            {
+                c = word[i].ToString();
+                randomWordEnd.Add(c);
+            }
+
+            string resultString = string.Join("", randomWordEnd);
+
+            Console.WriteLine(resultString);
+            Console.WriteLine(word);
+        }
+
+
+        private static string[] GetWords()
+        {
             var filename = "ordliste.txt";
             var data = File.ReadAllLines(filename);
-            var uniqueWords = "";
-            for (var index = 0; index < data.Length; index++)
+
+            var lastWord = string.Empty;
+            var uniqueWords = new List<string>();
+            foreach (var line in data)
             {
-                var line = data[index];
                 // Denne deler opp på tab mellomrom tror jeg?
-                string[] words = line.Split('\t');
+                string[] parts = line.Split('\t');
 
                 // Henter ut ordet jeg ønsker fra hver linje
+                var word = parts[1];
 
-                if (uniqueWords  == words[1]) continue;
-                uniqueWords  = words[1];
-                Console.WriteLine(uniqueWords );
+                if (word != lastWord
+                    && word.Length > 6
+                    && word.Length <= 10
+                    && !word.Contains("-"))
+                {
+                    uniqueWords.Add(word);
+                }
+
+                lastWord = word;
             }
+
+            return uniqueWords.ToArray();
         }
     }
 }
