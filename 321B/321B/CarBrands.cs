3e64b9b67dfef2cb03807af2500baa1f9044ed3e
martinsletsjoe <106916526+martinsletsjoe@users.noreply.github.com>
2023-11-30 10:39:33 +0100

    hæ

--- 321B/321B/CarBrands.cs
+++ 321B/321B/CarBrands.cs
@@ -2,13 +2,15 @@
 
 public class CarBrands
 {
-    public string CarBrand { get; set; }
-    public int Year { get; set; }
+    public string CarBrand { get; private set; }
+    public int Year { get; private set; }
+    public string[] CarTypes { get; private set; }
 
 
-    public CarBrands(string carBrand, int year)
+    public CarBrands(string carBrand, int year, string[] carTypes)
     {
         CarBrand = carBrand;
         Year = year;
+        CarTypes = carTypes;
     }
 }
--- 321B/321B/Program.cs
+++ 321B/321B/Program.cs
@@ -7,39 +7,38 @@
             // opprett array av bil-object
             CarBrands[] cars = new CarBrands[]
             {
-                new CarBrands("Toyota", 1990),
-                new CarBrands("Mercedes Benz", 1995),
-                new CarBrands("Volvo", 1994),
-                new CarBrands("Porsche", 1993),
-                new CarBrands("Bmw", 1992),
-                new CarBrands("Alfa Romeo", 1991),
+                new CarBrands("Toyota", 1990, new string[] {"Land Cruiser", "avensis"}),
+                new CarBrands("Mercedes Benz", 1995, new string[] {"300 SL", "E-Class"}),
+                new CarBrands("Volvo", 1994, new string[] {"xc70", "240"}),
+                new CarBrands("Porsche", 1993, new string[] {"911", "cayman"}),
+                new CarBrands("Bmw", 1992, new string[] {"3 Series", "5 Series"}),
+                new CarBrands("Alfa Romeo", 1991, new string[] {"gt-v6", "8C"}),
                 // Legg til biler
             };
 
             // Gå gjennom Arrayen og skriv ut informasjon om hver bil
-            foreach (CarBrands car in cars)
+            Console.WriteLine("Enter a car brand");
+            while (true)
             {
-                Console.WriteLine(car.CarBrand);
+                Show(cars);
             }
-
         }
 
-        //private static CarBrands[] CarArray()
-        //{
-
-        //    return car;
-        //}
+        private static void Show(CarBrands[] cars)
+        {
+            var userAnswer = Console.ReadLine();
+            foreach (CarBrands car in cars)
+            {
+                if (car.CarBrand.ToLower() == userAnswer.ToLower())
+                {
+                    Console.WriteLine($"Car Brand: {car.CarBrand}, year {car.Year},");
+                    Console.WriteLine("We have these in our inventory: ");
+                    for (int i = 0; i < car.CarTypes.Length; i++)
+                    {
+                        Console.WriteLine(" - "+ car.CarTypes[i]);
+                    }
+                }
+            }
+        }
     }
 }
-//public class CarBrands
-//{
-//    public string CarBrand { get; set; }
-//    public int Year { get; set; }
-
-
-//    public CarBrands(string carBrand, int year)
-//    {
-//        carBrand = carBrand;
-//        year = year;
-//    }
-//}
