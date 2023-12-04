e5adbda9106f059ddf014afddb9c159405bf8855
martinsletsjoe <106916526+martinsletsjoe@users.noreply.github.com>
2023-12-04 23:59:37 +0100

    a

---
+++ Colors/Colors/Color.cs
@@ -0,0 +1,73 @@
+﻿using System.Drawing;
+
+namespace Colors;
+
+public class Color
+{
+    private ConsoleColor _color;
+
+    public Color()
+    {
+        _color = ConsoleColor.Green;
+    }
+
+    public ConsoleColor GetCurrentColor()
+    {
+        return _color;
+    }
+
+    public void SetColors()
+    {
+        Console.BackgroundColor = _color;
+        Console.ForegroundColor = (ConsoleColor)(15 - (int)_color);
+    }
+
+    public void ToLight()
+    {
+        var colorName = _color.ToString();
+        if (colorName.Contains("Dark"))
+        {
+            colorName = colorName.Substring(4);
+            _color = Enum.Parse<ConsoleColor>(colorName);
+        }
+    }
+
+    public void ToDark()
+    {
+        var colorName = _color.ToString();  
+        colorName = "Dark" + colorName;
+        var isSuccess = ConsoleColor.TryParse(colorName, out _color);
+    }
+
+    public void InvertColor()
+    {
+        var colorNumber = (int)_color;
+        colorNumber = 15 - colorNumber;
+        _color = (ConsoleColor)colorNumber;
+    }
+
+    public void NextColor()
+    {
+        if ((_color == ConsoleColor.White))
+        {
+            _color = ConsoleColor.Black;
+        }
+        else
+        {
+            _color++;
+        }
+        
+    }
+
+    public void PrevColor()
+    {
+        if (_color == ConsoleColor.Black)
+        {
+            _color = ConsoleColor.White;
+        }
+        else
+        {
+            _color--;
+        }
+    }
+}
---
+++ Colors/Colors/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Colors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var color = new Color();
+
+            while (true)
+            {
+                color.SetColors();
+                Console.Clear();
+                Dictionary<string, Action<Color>> actions = new Dictionary<string, Action<Color>>                {
+                    {"1", c => c.ToLight()},
+                    {"2", c => c.ToDark()},
+                    {"3", c => c.InvertColor()},
+                    {"4", c => c.NextColor()},
+                    {"5", c => c.PrevColor()},
+                };
+                Console.WriteLine("Farge: " + color.GetCurrentColor());
+                Console.WriteLine("Hva vil du gjøre? ");
+                Console.WriteLine(" 1 = gå fra mørk til lys");
+                Console.WriteLine(" 2 = gå fra lys til mørk");
+                Console.WriteLine(" 3 = inverter ");
+                Console.WriteLine(" 4 = neste ");
+                Console.WriteLine(" 5 = forrige ");
+
+                var cmd = Console.ReadLine();
+                if (actions.ContainsKey(cmd))
+                {
+                    actions[cmd](color);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid command");
+                }
+            }
+
+            /*
+               Black = 0,
+               DarkBlue = 1,
+               DarkGreen = 2,
+               DarkCyan = 3,
+               DarkRed = 4,
+               DarkMagenta = 5,
+               DarkYellow = 6,
+               Gray = 7,
+               DarkGray = 8,
+               Blue = 9,
+               Green = 10,
+               Cyan = 11,
+               Red = 12,
+               Magenta = 13,
+               Yellow = 14,
+               White = 15
+             */
+        }
+    }
+}
